9de8155e4e95de83e3de8917717df8f55d10d24c
Muhammad Fajar <ochafajar@gmail.com>
2022-01-20 10:13:49 -0800

    first commit spring initializr

---
+++ src/main/java/com/example/antraTrainingSpringRest/AntraTrainingSpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.antraTrainingSpringRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntraTrainingSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntraTrainingSpringRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/antraTrainingSpringRest/AntraTrainingSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.antraTrainingSpringRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntraTrainingSpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
